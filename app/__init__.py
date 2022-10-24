e892902ca3f66fb90dc32d20528c427c3a0ca7c5
claire <yue.katou@gmail.com>
2022-10-24 09:44:53 -0700

    Building an API 1, Models and  Read

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
 def create_app():
-    # __name__ stores the name of the module we're in
-    app = Flask(__name__)
+  # __name__ stores the name of the module we're in
+  app = Flask(__name__)
 
-    from .routes.routes import bp
-    app.register_blueprint(bp)
+  from .routes import bp
+  app.register_blueprint(bp)
 
-    return app
+  return app
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,28 +1,28 @@
 from flask import Blueprint, jsonify
 
 class Cat:
-    def __init__(self, id, name, color, personality):
-        self.id = id
-        self.name = name
-        self.color = color
-        self.personality = personality
+  def __init__(self, id, name, color, personality):
+    self.id = id
+    self.name = name
+    self.color = color
+    self.personality = personality
 
 cats = [
-    Cat(1, "Luna", "grey", "naughty"), 
-    Cat(2, "Orange Cat", "orange", "antagonistic"),
-    Cat(3, "Big Ears", "grey and white", "sleepy")
+  Cat(1, "Luna", "grey", "naughty"), 
+  Cat(2, "Orange Cat", "orange", "antagonistic"),
+  Cat(3, "Big Ears", "grey and white", "sleepy")
 ]
 
 bp = Blueprint("cats", __name__, url_prefix="/cats")
 
 @bp.route("", methods=["GET"])
-def handle_cats():
-    results_list = []
-    for cat in cats: 
-        results_list.append(dict(
-            id=cat.id, 
-            name=cat.name, 
-            color=cat.color,
-            personality=cat.personality
-        ))
-    return jsonify(results_list)
+def get_all_cats():
+  results_list = []
+  for cat in cats: 
+    results_list.append({
+      "id": cat.id, 
+      "name": cat.name, 
+      "color": cat.color,
+      "personality": cat.personality
+    })
+  return jsonify(results_list)
