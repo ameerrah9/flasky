08150624c60d862cc5bd3697efb90fed67967df9
Ashley Yang <ashley.minghui@gmail.com>
2022-10-24 11:35:58 -0700

    2) Building an API - Read One and Errors

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Cat:
     def __init__(self, id, name, color, personality):
@@ -6,6 +6,14 @@ class Cat:
         self.name = name
         self.color = color
         self.personality = personality
+
+    def to_cat_dict(self):
+        return dict(
+            id=self.id, 
+            name=self.name, 
+            color=self.color,
+            personality=self.personality
+    )
 
 cats = [
     Cat(1, "Luna", "grey", "naughty"), 
@@ -19,10 +27,22 @@ bp = Blueprint("cats", __name__, url_prefix="/cats")
 def handle_cats():
     results_list = []
     for cat in cats: 
-        results_list.append(dict(
-            id=cat.id, 
-            name=cat.name, 
-            color=cat.color,
-            personality=cat.personality
-        ))
+        results_list.append(cat.to_cat_dict())
     return jsonify(results_list)
+
+def validate_cat(cat_id):
+    try:
+        cat_id = int(cat_id)
+    except:
+        abort(make_response({"message":f"cat {cat_id} invalid"}, 400))
+
+    for cat in cats: 
+        if cat.id == cat_id:
+            return cat
+
+    abort(make_response({"message":f"cat {cat_id} not found"}, 404))
+
+@bp.route("/<id>", methods=["GET"])
+def handle_cat(id):
+    cat = validate_cat(id)
+    return jsonify(cat.to_cat_dict())
