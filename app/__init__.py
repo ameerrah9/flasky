8051747cda85d4e81bf9e4bf478e2963e632d342
claire <yue.katou@gmail.com>
2022-10-24 12:25:55 -0700

    Building an API 2: Read One and Error Handling

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ def create_app():
   # __name__ stores the name of the module we're in
   app = Flask(__name__)
 
-  from .routes import bp
+  from .routes.routes import bp
   app.register_blueprint(bp)
 
   return app
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Cat:
   def __init__(self, id, name, color, personality):
@@ -15,6 +15,9 @@ cats = [
 
 bp = Blueprint("cats", __name__, url_prefix="/cats")
 
+#==============================
+#     GET ALL RESOURCES
+#==============================
 @bp.route("", methods=["GET"])
 def get_all_cats():
   results_list = []
@@ -26,3 +29,52 @@ def get_all_cats():
       "personality": cat.personality
     })
   return jsonify(results_list)
+
+#==============================
+#     GET ONE RESOURCE
+#==============================
+@bp.route("/<id>", methods=["GET"])
+def get_one_cat(id):
+  cat = validate_cat_id(id)
+
+  return jsonify({
+    "id": cat.id, 
+    "name": cat.name, 
+    "color": cat.color,
+    "personality": cat.personality
+  }), 200
+
+#=======================================
+# GET ONE RESOURCE -- CLUTTERED VERSION
+#=======================================
+# def get_one_cat(id):
+#   try:
+#     cat_id = int(id)
+#   except:
+#     return {"message": f"{id} is invalid"}, 400
+
+#   for cat in cats:
+#     if cat_id == cat.id:
+#       return jsonify({
+#         "id": cat.id, 
+#         "name": cat.name, 
+#         "color": cat.color,
+#         "personality": cat.personality
+#       }), 200
+      
+#   return {"message": f"{cat_id} not found"}, 404
+
+#==============================
+#     HELPER METHODS
+#==============================
+def validate_cat_id(id):
+  try:
+    cat_id = int(id)
+  except:
+    abort(make_response({"message":f"cat {id} is an invalid id"}, 400))
+
+  for cat in cats:
+    if cat_id == cat.id:
+      return cat
+      
+  abort(make_response({"message":f"cat {id} not found"}, 404))
