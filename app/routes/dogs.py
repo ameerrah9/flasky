0f00654826d6a82e8465bb471deae4a12b01f640
audrey <audreylandoy@gmail.com>
2022-10-24 08:45:18 -0700

    live code part 2 finished

--- app/routes/dogs.py
+++ app/routes/dogs.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 # Hardcode data
 class Dog:
@@ -20,16 +20,54 @@ dogs_bp = Blueprint('dogs_bp', __name__, url_prefix='/dogs')
 
 @dogs_bp.route('', methods=['GET'])
 def get_all_dogs():
-    dog_response = []
-    for dog in DOGS:
-        dog_response.append({
-            'id': dog.id,
-            'name': dog.name,
-            'age': dog.age,
-            'breed': dog.breed,
-            'gender': dog.gender
-        })
-
-    print(type(dog_response))
-    print(type(jsonify(dog_response)))
+    dog_response = [vars(dog) for dog in DOGS]
     return jsonify(dog_response)
+
+# GET ONE DOG? HOW?
+@dogs_bp.route('/<id>', methods=['GET'])     
+def get_one_dog(id):
+    # return dog as a dict
+    dog = validate_dog(id)
+    return dog
+
+# Validation function
+def validate_dog(id):
+    # handle invalid data types such as non-ints
+    try:
+        dog_id = int(id)
+    except ValueError:
+        return {
+            "message": "Invalid dog id"
+        }, 400
+
+    # handle if id is not found
+    for dog in DOGS:
+        if dog.id == dog_id:
+            return vars(dog)
+
+    abort(make_response(jsonify(description="Resource not found"),404)) 
+    
+    
+
+
+        
+
+
+
+# def get_all_dogs():
+    # dog_response = []
+    # for dog in DOGS:
+    #     print(vars(dog))
+    #     dog_response.append(vars(dog))
+        # dog_response.append({
+        #     'id': dog.id,
+        #     'name': dog.name,
+        #     'age': dog.age,
+        #     'breed': dog.breed,
+        #     'gender': dog.gender
+        # })
+
+    # print(DOGS)
+    # print(dog_response)
+    # print(type(dog_response))
+    # print(type(jsonify(dog_response)))
