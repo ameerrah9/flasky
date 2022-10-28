5543ce3e5f4133b5a8174928e6247b14b64ee0fe
Ashley Yang <ashley.minghui@gmail.com>
2022-10-28 10:46:11 -0700

    03) Building an API: Add POST /cats and GET /cats route

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,16 +1,8 @@
 from app import db
 from app.models.cat import Cat
-from flask import Blueprint, jsonify, abort, make_response
-
+from flask import Blueprint, jsonify, abort, make_response, request
 
 bp = Blueprint("cats", __name__, url_prefix="/cats")
-
-@bp.route("", methods=["GET"])
-def handle_cats():
-    results_list = []
-    for cat in cats: 
-        results_list.append(cat.to_cat_dict())
-    return jsonify(results_list)
 
 def validate_cat(cat_id):
     try:
@@ -24,8 +16,27 @@ def validate_cat(cat_id):
 
     abort(make_response({"message":f"cat {cat_id} not found"}, 404))
 
-@bp.route("/<id>", methods=["GET"])
-def handle_cat(id):
-    cat = validate_cat(id)
-    return jsonify(cat.to_cat_dict())
+# @bp.route("/<id>", methods=["GET"])
+# def handle_cat(id):
+#     cat = validate_cat(id)
+#     return jsonify(cat.to_cat_dict())
+
+@bp.route("", methods=["POST"])
+def create_cat():
+    request_body = request.get_json()
+    new_cat = Cat(name=request_body["name"],
+        color=request_body["color"],
+        personality=request_body["personality"])
+
+    db.session.add(new_cat)
+    db.session.commit()
+
+    return make_response(f"Cat {new_cat.name} successfully created", 201)
+
+@bp.route("", methods=["GET"])
+def read_all_cats():
+    cats = Cat.query.all()
+    cats_response = [cat.to_dict() for cat in cats]
+    return jsonify(cats_response)
+
 
