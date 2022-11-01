7fed2307a9eb279039811028397eeb44aac30728
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2022-11-01 10:09:45 -0400

    Add Read One, Update, Delete

--- app/routes/dogs.py
+++ app/routes/dogs.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from app import db
 from app.models.dog_model import Dog
 from flask import Blueprint, jsonify, make_response, request
@@ -8,7 +9,7 @@ dogs_bp = Blueprint('dogs_bp', __name__, url_prefix='/dogs')
 # ADD Get to Request
 @dogs_bp.route("", methods=["GET", "POST"])
 # Change name to handle dogs
-def handle_dog():
+def handle_dogs():
     if request.method == "GET":
         dogs = Dog.query.all()
         dogs_response = []
@@ -42,3 +43,40 @@ def handle_dog():
 
         # Successful response
         return make_response(f"Dog {new_dog.name} has been successfully created!", 201)
+
+# Path/Endpoint to get a single dog
+# Include the id of the record to retrieve as a part of the endpoint
+@dogs_bp.route("/<dog_id>", methods=["GET", "PUT", "DELETE"])
+# GET /dog/id
+def handle_dog(dog_id):
+    # Query our db to grab the dog that has the id we want:
+    dog = Dog.query.get(dog_id)
+
+    if request.method == "GET":
+        # Send back a single JSON object (dictionary):
+        return {
+            "id": dog.id,
+            "name": dog.name,
+            "breed": dog.breed,
+            "age": dog.age,
+            "gender": dog.gender
+        }
+    elif request.method == "PUT":
+        request_body = request.get_json()
+
+        # Updated dog attributes are set:
+        dog.name = request_body["name"],
+        dog.breed = request_body["breed"],
+        dog.age = request_body["age"],
+        dog.gender = request_body["gender"]
+
+        # Update this dog in the database
+        db.session.commit()
+
+        # Sucessful response
+        return make_response(f"Dog {dog.name} has been successfully updated!", 200)
+    elif request.method == "DELETE":
+        db.session.delete(dog)
+        db.session.commit()
+
+        return make_response(f"Dog {dog.name} successfully deleted", 202)
