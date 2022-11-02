0c4ab5ed33fae9ac7feb58e9c0d42481c4eb4d8d
Audrey Andoy <audreylandoy@gmail.com>
2022-11-02 10:41:46 -0400

    query params and srp for GET all and POST

--- app/routes/dogs.py
+++ app/routes/dogs.py
@@ -7,42 +7,66 @@ dogs_bp = Blueprint('dogs_bp', __name__, url_prefix='/dogs')
 
 # ADD Post to Request
 # ADD Get to Request
-@dogs_bp.route("", methods=["GET", "POST"])
+# http://localhost:5000/dogs?breed=dachshund&age=1
+@dogs_bp.route("", methods=["GET"])
 # Change name to handle dogs
 def handle_dogs():
-    if request.method == "GET":
-        dogs = Dog.query.all()
-        dogs_response = []
-        for dog in dogs:
-            dogs_response.append({
-                "id": dog.id,
-                "name": dog.name,
-                "breed": dog.breed,
-				"age": dog.age,
-				"gender": dog.gender
-            })
-        return jsonify(dogs_response)
-    elif request.method == "POST":
-        request_body = request.get_json()
+    dog_query = Dog.query
 
-        # Feel free to add a guard clause
-        if "name" not in request_body or "breed" not in request_body:
-            return make_response("Invalid Request, Name & Breed Can't Be Empty", 400)
-        # How we know about Dog
-        new_dog = Dog(
-        # You're looking for this key and assign it to name, breed, gender, age
-        name=request_body["name"],
-        breed=request_body["breed"],
-        age=request_body["age"],
-        gender=request_body["gender"]
+    breed_query = request.args.get("breed")
+    if breed_query:
+        # exact string matching - case sensitive
+        # dog_query = dog_query.filter_by(breed=breed_query)
+        # partial filters - case sensitive
+        # dog_query = dog_query.filter(Dog.breed.contains(breed_query))
+        # partial filter - case insensitive
+        dog_query = dog_query.filter(Dog.breed.ilike(f"%{breed_query}%"))
+
+    age_query = request.args.get("age")
+    if age_query:
+        dog_query = dog_query.filter_by(age=age_query)
+
+    dogs = Dog.query.all()
+
+    # dogs = Dog.query.filter_by(breed=breed_query).all()
+
+    dogs_response = []
+    for dog in dogs:
+        dogs_response.append({
+            "id": dog.id,
+            "name": dog.name,
+            "breed": dog.breed,
+            "age": dog.age,
+            "gender": dog.gender
+        })
+
+    if not dogs_response:
+        return make_response(jsonify(f"There are no {breed_query} dogs"))
+    return jsonify(dogs_response)
+
+@dogs_bp.route("", methods=["POST"])
+# Change name to handle dogs
+def create_dog():
+    request_body = request.get_json()
+
+    # Feel free to add a guard clause
+    if "name" not in request_body or "breed" not in request_body:
+        return make_response("Invalid Request, Name & Breed Can't Be Empty", 400)
+    # How we know about Dog
+    new_dog = Dog(
+    # You're looking for this key and assign it to name, breed, gender, age
+    name=request_body["name"],
+    breed=request_body["breed"],
+    age=request_body["age"],
+    gender=request_body["gender"]
     )
 
-        # Add this new instance of dog to the database
-        db.session.add(new_dog)
-        db.session.commit()
+    # Add this new instance of dog to the database
+    db.session.add(new_dog)
+    db.session.commit()
 
-        # Successful response
-        return make_response(f"Dog {new_dog.name} has been successfully created!", 201)
+    # Successful response
+    return make_response(f"Dog {new_dog.name} has been successfully created!", 201)
 
 # Path/Endpoint to get a single dog
 # Include the id of the record to retrieve as a part of the endpoint
