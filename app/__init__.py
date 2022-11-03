e975489ebb833341c733fda65001f8b4c96a43be
Mark Arciaga <mark.arciaga@adadevelopersacademy.org>
2022-11-03 13:32:15 -0700

    Fix SQLALCHEMY_DATABASE_URI key name in test app config.

--- app/__init__.py
+++ app/__init__.py
@@ -11,20 +11,20 @@ load_dotenv()
 def create_app(test_config=None):
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
-    
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     if not test_config:
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
     else:
         app.config["TESTING"] = True
-        app.config['SQLALCHEMY_TEST_DATABASE_URI'] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
 
     db.init_app(app)
     migrate.init_app(app, db)
-    
+
     from .routes.routes import bp
     app.register_blueprint(bp)
 
     from app.models.cat import Cat
-    
+
     return app
