a8edb23895708fcb8b304a7b381f23775a5a748c
Audrey Andoy <audreylandoy@gmail.com>
2022-11-07 14:41:55 -0500

    fin livecode solution with refactor

--- app/routes/caretaker_routes.py
+++ app/routes/caretaker_routes.py
@@ -1,6 +1,7 @@
 from app import db
 from app.models.caretaker import Caretaker
-from app.models.dog_model import Dog
+from app.models.dog import Dog
+from app.routes.routes_helper import *
 from flask import Blueprint, jsonify, abort, make_response, request
 
 caretaker_bp = Blueprint("caretaker_bp", __name__, url_prefix="/caretakers")
@@ -8,7 +9,7 @@ caretaker_bp = Blueprint("caretaker_bp", __name__, url_prefix="/caretakers")
 @caretaker_bp.route("", methods=["POST"])
 def create_caretaker():
     request_body = request.get_json()
-    new_caretaker = Caretaker(name=request_body["name"])
+    new_caretaker = Caretaker.from_dict(request_body)
 
     db.session.add(new_caretaker)
     db.session.commit()
@@ -18,34 +19,23 @@ def create_caretaker():
 @caretaker_bp.route("", methods=["GET"])
 def read_all_caretaker():
     caretakers = Caretaker.query.all()
-
-    caretaker_response = []
-    for caretaker in caretakers:
-        caretaker_response.append(
-            {
-                "id": caretaker.id,
-                "name": caretaker.name
-            }
-        )
+    caretaker_response = [caretaker.to_dict() for caretaker in caretakers]
     return jsonify(caretaker_response)
 
 # NESTED ROUTES
 @caretaker_bp.route("/<id>/dogs", methods=["POST"])
 def create_dog(id):
-    caretaker = Caretaker.query.get(id)
-
     request_body = request.get_json()
-    new_dog = Dog(
-        name=request_body["name"],
-        age=request_body["age"],
-        gender=request_body["gender"],
-        breed=request_body["breed"],
-        caretaker=caretaker)
+    caretaker = get_record_by_id(Dog, id)
 
+    new_dog = Dog.from_dict(request_body)
+    new_dog.caretaker = caretaker
+    
     db.session.add(new_dog)
     db.session.commit()
 
     return make_response(jsonify(f"Dog {new_dog.name} cared by {caretaker.name} successfully created"), 201)
+
 
 @caretaker_bp.route("/<id>/dogs", methods=["GET"])
 def read_dogs_of_caretaker(id):
