657c3a6c697ece08593ddc03dff0eee94b12b395
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2023-05-02 10:44:45 -0400

    Complete Read One, Update, Delete

--- app/routes.py
+++ app/routes.py
@@ -59,7 +59,21 @@ crystal_bp = Blueprint("crystals", __name__, url_prefix="/crystals")
 #         "color": crystal.color,
 #         "powers": crystal.powers
 #     }
+
+# responsible for validating and returning crytstal instance 
+def validate_crystal(crystal_id):
+    try:
+        crystal_id = int(crystal_id)
+    except:
+        abort(make_response({"message": f"{crystal_id} is not a valid type ({type(crystal_id)}). Must be an integer)"}, 400))
+
+    crystal = Crystal.query.get(crystal_id)
     
+    if not crystal:
+        abort(make_response({"message": f"crystal {crystal_id} does not exist"}, 404))
+        
+    return crystal
+
 @crystal_bp.route("", methods=['POST'])
 
 # define a route for creating a crystal resource
@@ -92,3 +106,52 @@ def read_all_crystals():
         })
     
     return jsonify(crystals_response)
+
+# define a route for getting a single crystal
+# GET /crystals/<crystal_id>
+@crystal_bp.route("/<crystal_id>", methods=["GET"])
+def read_one_crystal(crystal_id):
+    # Query our db to grab the crystal that has the id we want
+    crystal = validate_crystal(crystal_id)
+    
+    # show a single crystal
+    return {
+        "id": crystal.id,
+        "name": crystal.name,
+        "color": crystal.color,
+        "powers": crystal.powers
+    }, 200
+
+# define a route for updating a crystal
+# PUT /crystals/<crystal_id>
+@crystal_bp.route("/<crystal_id>", methods=["PUT"])
+def update_crystal(crystal_id):
+    # Query our db to grab the crystal that has the id we want
+    crystal = validate_crystal(crystal_id)
+    
+    request_body = request.get_json()
+    
+    crystal.name = request_body["name"]
+    crystal.color = request_body["color"]
+    crystal.powers = request_body["powers"]
+
+    db.session.commit()
+    
+    # send back the updated crystal
+    return {
+        "id": crystal.id,
+        "name": crystal.name,
+        "color": crystal.color,
+        "powers": crystal.powers
+    }, 200
+    
+# define a route for deleting a crystal
+# DELETE /crystals/<crystal_id>
+@crystal_bp.route("/<crystal_id>", methods=["DELETE"])
+def delete_crystal(crystal_id):
+    crystal = validate_crystal(crystal_id)
+    
+    db.session.delete(crystal)
+    db.session.commit()
+    
+    return make_response(f"Crystal #{crystal.id} successfully deleted")
