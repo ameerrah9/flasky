8548172299224d9af09058e194208abe00ce0b14
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2023-05-03 10:51:31 -0400

    Add Test Configurations

--- app/__init__.py
+++ app/__init__.py
@@ -3,18 +3,34 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 # import Migrate
 from flask_migrate import Migrate
+# import libraries for grabbing environment variables
+from dotenv import load_dotenv
+# used to read environment variables
+import os
 
 # gives use access to database operations
 db = SQLAlchemy()
 migrate = Migrate()
+# load the values from our .env file so the os module to be able to see them
+load_dotenv()
 
-def create_app():
+def create_app(test_config = None):
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
 
     # set up the database
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/flasky_development'
+    if not test_config:
+        # development environment configuration
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    else:
+        # test environment configuration
+        # if there is a test_config passed in,
+        # this means we're trying to test the app,
+        # configure the test settings
+        app.config["TESTING"] = True
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
 
     # connect the db and migrate to our flask app
     db.init_app(app)
